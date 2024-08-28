60f9e8f1ad6ec0b11393702f04acd0b6a3e62e5a
Bharat Kushwah <bharatkushwah407@gmail.com>
2024-08-28 18:23:39 +0530

    Carousel img added

--- src/pages/Accomdation.jsx
+++ src/pages/Accomdation.jsx
@@ -18,6 +18,7 @@ const Carousel = () => {
     "https://images.jdmagicbox.com/bangalore/n8/080pxx80.xx80.170420183157.q2n8/cbnr/f1eb937d8c8f676d841feada5ce42ec3.jpg",
     "https://images.jdmagicbox.com/bangalore/n8/080pxx80.xx80.231127172703.a6n8/cbnr/74fce72925ccf71ca14f584cdfdd41e5.jpg",
     "https://images.jdmagicbox.com/bangalore/l3/080pxx80.xx80.121110191448.p3l3/cbnr/26cd8fde3eb30d839bbf101196222d4b.jpg",
+    "https://images.jdmagicbox.com/bangalore/m3/080pxx80.xx80.190821143520.h4m3/cbnr/4871777756ef945b07b6c2545cfabef6.jpg",
   ];
 
   return (
