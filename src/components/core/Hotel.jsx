b97388b7865c6d3c56743dc5311c568357a78232
Bharat Kushwah <bharatkushwah407@gmail.com>
2024-09-30 12:36:16 +0530

    css of real page done

--- src/components/core/Hotel.jsx
+++ src/components/core/Hotel.jsx
@@ -25,7 +25,7 @@ const HotelListing = () => {
   } = Hotel;
 
   return (
-    <div className="max-w-[1400px] mt-4 mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="max-w-[1400px] flex lg:flex-row flex-col bg-white shadow-lg gap-2 py-2 items-center px-2 mx-2 w-full md:w-[72%] rounded-lg overflow-x-hidden">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <div className="flex flex-col sm:flex-row p-4 sm:p-6">
           <div className="w-full sm:w-1/3 lg:w-1/4 mb-4 sm:mb-0">
--- src/components/section/FilterSection.jsx
+++ src/components/section/FilterSection.jsx
@@ -6,7 +6,7 @@ const FilterSection = ({ heading }) => {
   return (
     <div className="flex flex-col ">
       <h2 className="text-3xl mx-4 my-2 mt-2 font-semibold">{heading}</h2>
-      <div className="flex mx-2">
+      <div className="flex flex-wrap mx-2">
         <Dropdrown text="Sort by Rating" />
         <FilterButton text="Top Rated" />
         <Dropdrown text="State" />
